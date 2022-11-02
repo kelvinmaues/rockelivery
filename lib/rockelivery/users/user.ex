1fc85e04cc896916321dc4f9c2ddb6a598eb639a
Kelvin Maués <kgmdeveloper@gmail.com>
2022-11-02 12:53:12 -0300

    feat(schema): validate user changeset

--- lib/rockelivery/users/user.ex
+++ lib/rockelivery/users/user.ex
@@ -1,10 +1,10 @@
 defmodule Rockelivery.Users.User do
   use Ecto.Schema
-
   import Ecto.Changeset
+  alias Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: true}
-  @required_params [:address, :age, :zip_code, :document_number, :email, :password_hash, :name]
+  @required_params [:address, :age, :zip_code, :document_number, :email, :password, :name]
 
   schema "users" do
     field :address, :string
@@ -12,6 +12,7 @@ defmodule Rockelivery.Users.User do
     field :zip_code, :string
     field :document_number, :string
     field :email, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
     field :name, :string
 
@@ -21,5 +22,22 @@ defmodule Rockelivery.Users.User do
   def changeset(params) do
     %__MODULE__{}
     |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:password_hash, min: 6)
+    |> validate_length(:zip_code, is: 8)
+    |> validate_length(:document_number, is: 11)
+    |> validate_number(:age, greater_than_or_equal_to: 18)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint([:email])
+    |> unique_constraint([:document_number])
+    |> put_password_hash()
   end
+
+  defp put_password_hash(
+         %Changeset{valid?: true, changes: %{password: password}} = change_set
+       ) do
+    change(change_set, Pbkdf2.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
