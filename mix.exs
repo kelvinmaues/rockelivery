675d92d600e2d2fc59f60547751b463dc3557c75
Kelvin Maués <kgmdeveloper@gmail.com>
2022-11-02 12:52:30 -0300

    feat(dependency): hash password with new library

--- mix.exs
+++ mix.exs
@@ -45,6 +45,7 @@ defmodule Rockelivery.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:pbkdf2_elixir, "~> 2.0"},
     ]
   end
 
